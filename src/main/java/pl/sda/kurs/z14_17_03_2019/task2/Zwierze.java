bea7e20c46e5b00523d17c2691f72d408e63878b
Radek <roszko.radek@gmail.com>
2019-03-30 09:31:52 +0100

    jeszcze raz przed zajeciami

--- src/main/java/pl/sda/kurs/z14_17_03_2019/task2/Zwierze.java
+++ src/main/java/pl/sda/kurs/z14_17_03_2019/task2/Zwierze.java
@@ -1,6 +1,6 @@
 package pl.sda.kurs.z14_17_03_2019.task2;
 
-public interface gitZwierze {
+public interface Zwierze {
     void dajGlos();
     default void przedstawSie(){
         System.out.println(getClass().getName());
